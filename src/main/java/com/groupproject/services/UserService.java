b6d506537a96133c8c327d5b4d3e42b5eb9b4bf6
Jazmine Spann <jazminespann101@gmail.com>
2021-09-24 21:40:18 -0500

    added user services

---
+++ src/main/java/com/groupproject/services/UserService.java
@@ -0,0 +1,61 @@
+package com.groupproject.services;
+
+import java.util.Optional;
+
+import org.mindrot.jbcrypt.BCrypt;
+import org.springframework.stereotype.Service;
+
+import com.groupproject.models.User;
+import com.groupproject.repositories.UserRepository;
+
+@Service
+public class UserService {
+	private final UserRepository userRepository;
+	
+	public UserService(UserRepository userRepository) { 
+		this.userRepository= userRepository;
+	}
+	// register user:  hash their password
+		public User registerUser(User user) { 
+			String hashed = BCrypt.hashpw(user.getPassword(), BCrypt.gensalt());
+			user.setPassword(hashed);
+			return userRepository.save(user);
+		}
+
+		//find user by email
+		public User findByEmail(String email) { 
+			return userRepository.findByEmail(email);
+		}
+		
+//		//find user by name
+//			public User findByName(String name) {
+//				return userRepository.findByName(name);
+//			}
+		//find user by id
+		public User findUserById(Long id) { 
+			Optional<User> optional = userRepository.findById(id);
+			if(optional.isPresent()) {
+				return optional.get();
+			}else {
+				return null;
+			}
+		}
+
+
+		//authenticate user 
+		public boolean authenticateUser(String email, String password) { 
+			 // first find the user by email
+		    User user = userRepository.findByEmail(email);
+		    // if we can't find it by email, return false
+		    if(user == null) {
+		        return false;
+		    } else {
+		        // if the passwords match, return true, else, return false
+		        if(BCrypt.checkpw(password, user.getPassword())) {
+		            return true;
+		        } else {
+		            return false;
+		        }
+		    }
+		}
+}
