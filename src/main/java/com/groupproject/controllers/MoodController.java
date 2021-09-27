bd8445969d31e93fef97e150d29764c81f57c63a
Jazmine Spann <jazminespann101@gmail.com>
2021-09-26 23:48:17 -0500

    added mapping for dashboard

---
+++ src/main/java/com/groupproject/controllers/MoodController.java
@@ -0,0 +1,23 @@
+package com.groupproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.groupproject.services.UserService;
+import com.groupproject.validator.UserValidator;
+
+@Controller
+public class MoodController {
+	private final UserService userService;
+	private final UserValidator userValidator;
+
+	public MoodController(UserService userService, UserValidator userValidator) {
+		this.userService = userService;
+		this.userValidator = userValidator;
+	}
+	
+	@RequestMapping("/dashboard")  //jazmine working on view
+	public String dashboard() {
+		return "dashboard.jsp";
+	}
+}
