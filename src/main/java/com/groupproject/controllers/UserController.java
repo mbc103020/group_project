0c8175b9d43792e15f79531630621a292a44e880
Jazmine Spann <jazminespann101@gmail.com>
2021-09-30 16:03:24 -0500

    added list of states

--- src/main/java/com/groupproject/controllers/UserController.java
+++ src/main/java/com/groupproject/controllers/UserController.java
@@ -27,7 +27,9 @@ public class UserController {
 		this.userValidator = userValidator;
 	}
 	
-	
+	 public static final String[] states = {
+	    		"AL", "AK", "AZ", "AR", "CA", "CO", "CT", "DE", "FL", "GA", "HI", "ID", "IL", "IN", "IA", "KS", "KY", "LA", "ME", "MD", "MA", "MI", "MN", "MS", "MO", "MT", "NE", "NV", "NH", "NJ", "NM", "NY", "NC", "ND", "OH", "OK", "OR", "PA", "RI",	"SC", "SD", "TN", "TX", "UT", "VT", "VA", "WA", "WV", "WI", "WY"
+	    };
 
     @PostMapping("/registration")
     public String registerUser(@Valid @ModelAttribute("user") User user, BindingResult result, HttpSession session, Model model) {
